24b931b11022e07ae9010e9adfd2008af655b646
Damian <damianpud@gmail.com>
2020-11-13 17:08:51 +0100

    REST framework

--- courses/urls.py
+++ courses/urls.py
@@ -1,7 +1,11 @@
-from django.urls import path
+from django.urls import path, include
+
+from rest_framework.routers import DefaultRouter
 
 from courses.views import CourseCreateView, CourseDetailView, CourseUpdateView, CourseDeleteView,\
     CourseListView
+
+router = DefaultRouter()
 
 app_name = 'courses'
 urlpatterns = [
@@ -10,4 +14,5 @@ urlpatterns = [
     path('course/update/<pk>', CourseUpdateView.as_view(), name='course_update'),
     path('course/delete/<pk>', CourseDeleteView.as_view(), name='course_delete'),
     path('course/detail/<pk>', CourseDetailView.as_view(), name='course_detail'),
+    path('api/', include(router.urls))
 ]
--- sdaworld/settings.py
+++ sdaworld/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'courses',
     'bootstrap4',
     'crispy_forms',
+    'rest_framework',
     'accounts'
 ]
 
