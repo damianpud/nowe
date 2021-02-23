e5bd79588d9f700767211fc15ec640a90976d411
Damian <damianpud@gmail.com>
2021-02-23 12:05:41 +0100

    updated course extra

--- courses/templatetags/course_extra.py
+++ courses/templatetags/course_extra.py
@@ -9,11 +9,17 @@ register = Library()
 def course_format(course, short=False):
     if short:
         return f'{course.title} ({course.technology})'
-    return f'{course.title} ({course.technology}) - {course.price}'
+    return f'{course.title} ({course.technology}) - {course.price} PLN'
 
 
 @register.filter
 def attr_as_p(obj, attrname):
     label = escape(attrname.capitalize())
     value = escape(getattr(obj, attrname))
+    if label == 'File':
+        return SafeString(f'<p><strong>{label}:</strong> <a href="/media/{value}">{value}</a></p>')
+    elif label == 'Description':
+        return SafeString(f'<p><strong>{label}:</strong><p><p>{value}</p>')
+    elif label == 'Price':
+        return SafeString(f'<p><strong>{label}:</strong> {value} PLN</p>')
     return SafeString(f'<p><strong>{label}:</strong> {value}</p>')
