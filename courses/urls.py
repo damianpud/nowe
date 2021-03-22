9e836420b173331b71cb1a33b9899fb733e05883
Damian <damianpud@gmail.com>
2021-03-22 12:37:37 +0100

    updated urls

--- courses/urls.py
+++ courses/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
 from courses.views import CourseCreateView, CourseDetailView, CourseUpdateView, CourseDeleteView,\
-    CourseListView, TechnologyViewSet, CourseViewSet
+    CourseListView, TechnologyViewSet, CourseViewSet, OwnerCourseListView
 
 router = DefaultRouter()
 router.register('technology', TechnologyViewSet)
@@ -12,6 +12,7 @@ router.register('course', CourseViewSet)
 app_name = 'courses'
 urlpatterns = [
     path('course/list', CourseListView.as_view(), name='course_list'),
+    path('course/owner_courses_list', OwnerCourseListView.as_view(), name='owner_courses_list'),
     path('course/create', CourseCreateView.as_view(), name='course_create'),
     path('course/update/<pk>', CourseUpdateView.as_view(), name='course_update'),
     path('course/delete/<pk>', CourseDeleteView.as_view(), name='course_delete'),
